d4a1dddb5ad04faf4319e0e71cd3036abf65431e
nico147 <normalboyinlife@gmail.com>
2023-01-24 00:49:11 +0200

    Nearly done GUI

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from scripts.parameters import *
 
-
 class app():
     def __init__(self) -> None:        
-        self.update_display_elements()        
-        self.draw_wall = False
+        self.update_display_elements()
+        self.is_menu_on = False
+        self.draw_obstacle = False
         self.start_node = None
+        self.end_node = None
         self.draw_start = False
+        self.draw_end = False
             
     def get_screen_size(self):
         self.current_w = pygame.display.Info().current_w
@@ -14,7 +16,7 @@ class app():
     
     def update_display_elements(self, node_size=default_node_size):
         self.get_screen_size()
-        self.wall = []
+        self.obstacle = []
         self.node_size = node_size
         self.x_padding = self.current_w % self.node_size / 2
         self.y_padding = self.current_h % self.node_size / 2
@@ -24,9 +26,8 @@ class app():
         size = (self.node_size, self.node_size)
         self.grid = [ [Rect((self.x_padding + columne_index*self.node_size, self.y_padding + row_index*self.node_size), size) for row_index in range(self.grid_rows)] for columne_index in range(self.grid_columes) ]
         self.tgrid = [ [(self.x_padding + columne_index*self.node_size, self.y_padding + row_index*self.node_size) for row_index in range(self.grid_rows)] for columne_index in range(self.grid_columes) ]
-        print(self.grid_rows, self.grid_columes)
-        print(self.tgrid)
-        
+        # print(self.grid_rows, self.grid_columes)
+        # print(self.tgrid)        
     
     def draw_borders(self, screen):
         pygame.draw.rect(screen, BLACK, Rect((0,0), (self.current_w, self.y_padding)))
@@ -39,22 +40,38 @@ class app():
         for index in range(self.grid_rows+1):
             pygame.draw.line(screen, WHITE, (0, self.y_padding + index*self.node_size), (self.current_w, self.y_padding + index*self.node_size))    
     
-    def draw_nodes(self, screen):        
+    def draw_nodes(self, screen):
+        if not self.is_menu_on:            
+            if self.draw_start:
+                if pygame.mouse.get_pressed()[0]:
+                    self.start_node = self.mouse_interact()
+            if self.draw_end:
+                if pygame.mouse.get_pressed()[0]:
+                    self.end_node = self.mouse_interact()
+            if self.draw_obstacle: 
+                if pygame.mouse.get_pressed()[0]:
+                    selection = self.mouse_interact()
+                    if selection not in self.obstacle and len(selection) == 2:
+                        self.obstacle.append(selection)
+                if pygame.mouse.get_pressed()[2]:
+                    selection = self.mouse_interact()
+                    if selection in self.obstacle and len(selection) == 2:
+                        self.obstacle.remove(selection)
+        if self.obstacle:
+            for cell in self.obstacle:
+                pygame.draw.rect(screen, BLACK, self.grid[cell[0]][cell[1]])
         if self.start_node:
+            if self.start_node in self.obstacle:
+                self.obstacle.remove(self.start_node)
             row = self.start_node[0]
             colume = self.start_node[1]
-            pygame.draw.rect(screen, ORANGE, self.grid[row][colume])            
-        if self.draw_wall: 
-            if pygame.mouse.get_pressed()[0]:
-                selection = self.mouse_interact()
-                if selection not in self.wall and len(selection) == 2:
-                    self.wall.append(selection)
-        if self.wall:
-            for cell in self.wall:
-                pygame.draw.rect(screen, BLACK, self.grid[cell[0]][cell[1]])
-                
-    def draw_start_node(self):        
-        pass
+            pygame.draw.rect(screen, ORANGE, self.grid[row][colume])
+        if self.end_node:
+            if self.end_node in self.obstacle:
+                self.obstacle.remove(self.end_node)
+            row = self.end_node[0]
+            colume = self.end_node[1]
+            pygame.draw.rect(screen, GREEN, self.grid[row][colume])
 
     def mouse_interact(self):            
         mouse_pos_x = pygame.mouse.get_pos()[0]
@@ -70,23 +87,106 @@ class app():
         # print(result)
         # print(self.tgrid[result[0]][result[1]])
         return result
-   
-# test = Button('Test')
-# test.draw(screen, 250, 100, (400,500), 10)
+    
+    def reset(self, reset_signal):        
+        if reset_signal:
+            self.obstacle = []
+            self.start_node = None
+            self.end_node = None            
+        return False  # toggle off the reset signal
+
+class menu():
+    def __init__(self) -> None:        
+        self.button_list = {}
+        self.get_screen_size()
+                                         
+        self.button_list["START"] = Button("PICK START", btn_highlight=GREEN, cb=[self.toggle_input, "START"])
+        self.button_list["END"] = Button("PICK END", btn_highlight=RED, cb=[self.toggle_input, "END"])
+        self.button_list["OBSTACLE"] = Button("DRAW OBSTACLE", cb=[self.toggle_input, "OBSTACLE"])        
+        self.button_list["RESET"] = Button("RESET", cb=[self.reset])
+        
+        self.menu_reveal = False
+        self.in_progress = None
+        self.reset_signal = False
+        self.draw_obstacles = False
+        self.draw_start = False
+        self.draw_end = False
+    
+    def get_screen_size(self):
+        self.current_w = pygame.display.Info().current_w
+        self.current_h = pygame.display.Info().current_h
+        
+        self.start_pos = (self.current_w//4, self.current_h//4)
+        self.button_width = 250
+        self.button_height = 40
+        self.button_elevation = 5
+        self.offset = (self.start_pos[1] + (len(self.button_list) * self.button_height)) // (len(self.button_list) + 1) if self.button_list else 50
+        
+        panel_size = (self.button_width + 200, self.button_height*len(self.button_list) + self.offset*(len(self.button_list)-1) + 200)
+        self.panel = pygame.Surface(panel_size)
+        self.panel.fill(DARKJADE)
+        self.panel.set_alpha(120)
+    
+    def DrawButton(self, screen):
+        index = 0
+        x = self.start_pos[0]
+        for button in self.button_list:
+            new_button_pos = (x, self.start_pos[1] + index * self.offset)
+            if self.in_progress == button or not self.in_progress:
+                self.button_list[button].draw(screen, self.button_width, self.button_height, new_button_pos, self.button_elevation)
+            else:
+                self.button_list[button].draw(screen, self.button_width, self.button_height, new_button_pos, self.button_elevation, True)
+            index += 1
+    
+    def draw_menu_panel(self, screen):
+        self.get_screen_size()
+        if self.menu_reveal:            
+            screen.blit(self.panel, (self.start_pos[0]-100,self.start_pos[1]-100))
+            self.DrawButton(screen)        
+
+    def toggle_input(self, input):
+        if self.in_progress and self.in_progress != input:
+            return
+        if input == "START":
+            self.draw_start = not self.draw_start            
+            self.in_progress = input if self.draw_start else None
+        if input == "END":
+            self.draw_end = not self.draw_end
+            self.in_progress = input if self.draw_end else None
+        if input == "OBSTACLE":
+            self.draw_obstacles = not self.draw_obstacles
+            self.in_progress = input if self.draw_obstacles else None
+    
+    def toggle_menu(self):
+        self.menu_reveal = not self.menu_reveal
+        
+    def confirm_button_callback(self):
+        self.toggle_input(self.in_progress)
+    
+    def reset(self):
+        self.reset_signal = True
+
 
 if __name__ == '__main__':
     app = app()
-    
-    while running:                                
-        draw_start = False            
-        for event in pygame.event.get():
+    menu = menu()
+    while running:                                        
+        for event in pygame.event.get():            
             if event.type == QUIT:
-                running = False
-            # if event.type == MOUSEBUTTONUP:
-            #     app.start_node = app.mouse_interact()
+                running = False            
             if event.type == KEYDOWN:
                 if event.key == K_s:
-                    app.draw_wall = not app.draw_wall
+                    menu.toggle_input("START")
+                if event.key == K_e:
+                    menu.toggle_input("END")
+                if event.key == K_o:
+                    menu.toggle_input("OBSTACLE")
+                if event.key == K_BACKSPACE:
+                    menu.reset()
+                if event.key == K_RETURN:
+                    menu.confirm_button_callback()
+                if event.key == K_ESCAPE:
+                    menu.toggle_menu()
                 if event.key == K_f:
                     if not fullscreen_flag:                    
                         screen = pygame.display.set_mode((0,0), FULLSCREEN)
@@ -94,11 +194,24 @@ if __name__ == '__main__':
                         screen = pygame.display.set_mode((default_width, default_height))
                     fullscreen_flag = not fullscreen_flag
                     app.update_display_elements()
+        # Update menu selection
+        app.is_menu_on = menu.menu_reveal
+        app.draw_obstacle = menu.draw_obstacles
+        app.draw_start = menu.draw_start
+        app.draw_end = menu.draw_end
+        menu.reset_signal = app.reset(menu.reset_signal)
         
+        # Background
         screen.fill(OATYELLOW)        
+        # Draw main section
         app.draw_borders(screen)
         app.draw_nodes(screen)
-        debug(f"{pygame.mouse.get_pos()}", pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+        # Draw menu section
+        menu.draw_menu_panel(screen)
+        
+        # debug(f"{pygame.mouse.get_pos()}", pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+        
+        debug(f"{menu.in_progress if menu.in_progress else ''}", pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
         pygame.display.update()
         clock.tick(fps)    
 
--- scripts/parameters.py
+++ scripts/parameters.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, os
 from pygame.locals import *
 
 pygame.init()
@@ -9,11 +9,13 @@ clock = pygame.time.Clock()
 # Color variables
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 DARKRED = "#480113"
 BLACK = (0, 0, 0)
 GREY = "#808080"
 DARKBLUE = "#054569"
 LIGHTBLUE = "#8ecae6"
+DARKJADE = "#006666"
 MIDBLUE = "#219ebc"
 OATYELLOW = "#f1e3bc"
 HONEYYELLOW = "#ffb703"
@@ -27,18 +29,17 @@ fullscreen_flag = False
 screen = pygame.display.set_mode((default_width, default_height))
 running = True
 fps = 60
-font = pygame.font.SysFont("comicsans", 30)
-default_node_size = 100
+font = pygame.font.SysFont("Arial", 30)
+default_node_size = 50
 
-
-grid = [100,100]
 
 class Button:
-    def __init__(self, text, btn_color="#ced7e0", btn_highlight="#9ccddc", btn_shadow="#054569", btn_textSize=30, btn_textColor="#062c43", cb=None):
+    def __init__(self, text, btn_color="#ced7e0", btn_highlight="#9ccddc", btn_shadow="#054569", btn_textSize=30, btn_textColor="#062c43", cb=[]):
         #Core attributes 
         self.btn_attributes = [btn_color, btn_highlight, btn_shadow]
         self.pressed = False		                                               
-        self.gui_font = pygame.font.Font(None,btn_textSize)
+        # self.gui_font = pygame.font.Font(os.path.join(os.getcwd(), "Assets", "Fonts", "monogram.ttf"), btn_textSize)
+        self.gui_font = pygame.font.SysFont("monaco", btn_textSize)
         self.top_color = self.btn_attributes[0]        		
         self.bottom_color = self.btn_attributes[2]
         self.text = text
@@ -47,7 +48,7 @@ class Button:
         #text
         self.text_surf = self.gui_font.render(text,True,btn_textColor)
 
-    def draw(self, screen, width, height, pos, elevation):
+    def draw(self, screen, width, height, pos, elevation, disable=False):
         # Okay
         self.elevation = elevation        
         self.original_y_pos = pos[1]                		                
@@ -63,31 +64,34 @@ class Button:
         self.top_rect.y = self.original_y_pos - self.elevation         
 
         self.bottom_rect.midtop = self.top_rect.midtop        
-        self.bottom_rect.y = self.original_y_pos
-        self.check_click(screen)                       
+        self.bottom_rect.y = self.original_y_pos        
+        self.check_click(screen, disable)
 
-    def check_click(self, screen):
+    def check_click(self, screen, disable=False):
         mouse_pos = pygame.mouse.get_pos()
-        if self.top_rect.collidepoint(mouse_pos):
-            self.top_color = self.btn_attributes[1]			
+        if not disable:
+            if self.top_rect.collidepoint(mouse_pos):
+                self.top_color = self.btn_attributes[1]			
 
-            if pygame.mouse.get_pressed()[0]:                
-                self.top_rect.y = self.original_y_pos
-                self.pressed = True                
+                if pygame.mouse.get_pressed()[0]:                
+                    self.top_rect.y = self.original_y_pos
+                    self.pressed = True                
+                else:            
+                    self.top_rect.y = self.original_y_pos - self.elevation
+                    if self.pressed == True:					                                                          
+                        if self.callback != []:
+                            if len(self.callback) > 1:
+                                self.callback[0](*self.callback[1:])
+                            else:
+                                self.callback[0]()
+                        self.pressed = False                    
             else:            
                 self.top_rect.y = self.original_y_pos - self.elevation
-                if self.pressed == True:					                                                          
-                    if self.callback != None:
-                        self.callback()                    
-                    self.pressed = False                    
-        else:            
-            self.top_rect.y = self.original_y_pos - self.elevation
-            self.top_color = self.btn_attributes[0]
+                self.top_color = self.btn_attributes[0]
         self.text_rect.center = self.top_rect.center
         pygame.draw.rect(screen,self.bottom_color, self.bottom_rect,border_radius = 12)
         pygame.draw.rect(screen,self.top_color, self.top_rect,border_radius = 12)
         screen.blit(self.text_surf, self.text_rect)
-
 
 def debug(info, x=10, y=10):
     display_surf = pygame.display.get_surface()
